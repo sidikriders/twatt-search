082c204ab60eeeced9b2887842a77567455b2c74
sidikriders <your@example.com>
2017-05-24 12:37:49 +0700

    kurang dot env

--- controller/twattControl.js
+++ controller/twattControl.js
@@ -1,3 +1,6 @@
+const OAuth = require('oauth');
+
+
 function searchTwat(req, res, next) {
   let key = req.headers.key
   let keyword = key.split(' ').join('%20')
@@ -5,24 +8,56 @@ function searchTwat(req, res, next) {
   var oauth = new OAuth.OAuth(
     'https://api.twitter.com/oauth/request_token',
     'https://api.twitter.com/oauth/access_token',
-    process.env.APP_CONS_KEY,
-    // 'BjW6fAJLGmCXX1QjOp3KtQqPW',
-    'AuuwqNwJzOPHOpSH6RSBf61sLQ2KL9TOyD9M1Tu6pui5z4tClb',
+    // process.env.APP_CONS_KEY,
+    'Y0DFClhw4wEPLzBwryFjxf302',
+    'xGA5VcPcYV1B6tnX1clYgn20E5LYnbNGUzWDdwjsCLaxq74o1h',
     '1.0A',
     null,
     'HMAC-SHA1'
   )
 
   oauth.get(
-      `https://api.twitter.com/1.1/search/tweets.json?q=%40${keyword}&src=typd`,
+      `https://api.twitter.com/1.1/search/tweets.json?q=${keyword}`,
       '320621031-a9AL3hth99QLzz5Ybq7HkQfqqm0Q94pxd8lHoxor', //test user token
       'q9rkQmtdrWk6xrJQBxbNwgEUgNyDkxGSbD69dDcr7Fgjx', //test user secret
       function (e, data){
         if (e) console.error(e);
-        res.send(data)
+        let obj = JSON.parse(data).statuses
+        let text = []
+        obj.forEach( x => {
+          text.push({"text": x.text})
+        })
+        res.send(text)
+      })
+}
+
+function homeTL(req, res, next) {
+  let username = req.headers.username
+  var oauth = new OAuth.OAuth(
+    'https://api.twitter.com/oauth/request_token',
+    'https://api.twitter.com/oauth/access_token',
+    // process.env.APP_CONS_KEY,
+    'Y0DFClhw4wEPLzBwryFjxf302',
+    'xGA5VcPcYV1B6tnX1clYgn20E5LYnbNGUzWDdwjsCLaxq74o1h',
+    '1.0A',
+    null,
+    'HMAC-SHA1'
+  )
+
+  oauth.get(
+      `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${username}&count=5`,
+      '320621031-a9AL3hth99QLzz5Ybq7HkQfqqm0Q94pxd8lHoxor', //test user token
+      'q9rkQmtdrWk6xrJQBxbNwgEUgNyDkxGSbD69dDcr7Fgjx', //test user secret
+      function (e, data){
+        if (e) console.error(e);
+        let tl = JSON.parse(data).map( x => {
+          return 'Tweet: ' + x.text
+        })
+        res.send(tl)
       })
 }
 
 module.exports = {
-  searchTwat
+  searchTwat,
+  homeTL
 }
--- routes/index.js
+++ routes/index.js
@@ -3,3 +3,7 @@ var router = express.Router();
 var fung = require('../controller/twattControl.js')
 
 router.get('/', fung.searchTwat)
+
+router.get('/home', fung.homeTL)
+
+module.exports=router;
